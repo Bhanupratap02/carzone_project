99c41969de541533a24f5c900d309804b8d92aa6
Bhanu pratap Behera <bhanupratapbehera193@gmail.com>
2024-10-18 18:42:35 +0530

    added cars app and displayed in frontend

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Car
+from django.utils.html import format_html
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(selft,object):
+        return format_html('<img src="{}" width="40" style="border-radius:50%" />'.format(object.car_photo.url))
+    thumbnail.short_description = 'Photo'
+    list_display = ('id','thumbnail','car_title','state','model','body_style','passengers','fuel_type','is_featured',)
+    list_display_links = ('id','thumbnail','car_title')
+    list_editable = ('is_featured',)
+    search_fields = ('id','car_title','model','body_style','fuel_type')
+    list_filter = ('model','body_style','fuel_type','state',)
+admin.site.register(Car,CarAdmin)
